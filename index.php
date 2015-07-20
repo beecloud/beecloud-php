b02446a03f44d13e8a2411db83f96f099cf790f1
Danzeer <kaevanka@gmail.com>
2015-07-20 09:56:42 +0800

    add index

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dengze
- * Date: 7/17/15
- * Time: 15:11
- */
-
-//TODO:
+<html>
+<head>
+    <meta content="text/html;charset=UTF-8">
+</head>
+<body>
+<h1>BeeCloud Demo</h1>
+<ul>
+    <li><a href="demo/ali.web.php">支付宝即使到账</a></li>
+    <li><a href="demo/ali.qrcode.php">支付宝扫码</a></li>
+    <li><a href="demo/ali.bills.php">支付宝订单查询和退款</a></li>
+    <li><a href="demo/ali.refunds.php">支付宝退款查询</a></li>
+    <li><a href="demo/un.web.php">银联网页收银台</a></li>
+    <li><a href="demo/un.bills.php">银联订单查询和退款</a></li>
+    <li><a href="demo/un.refunds.php">银联退款查询</a></li>
+    <li><a href="demo/wx.native.php">微信扫码</a></li>
+    <li><a href="demo/wx.jsapi.php">微信公众号支付(微信浏览器打开)</a></li>
+    <li><a href="demo/wx.bills.php">微信订单查询和退款</a></li>
+    <li><a href="demo/wx.refunds.php">微信退款查询和退款状态更新</a></li>
+</ul>
+</body>
+</html>
